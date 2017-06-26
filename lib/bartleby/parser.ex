602a8fbfce5991979a7f66c51b7fe0ab9cfe1c75
Bobby Grayson <bobbygrayson@gmail.com>
2017-06-26 15:50:30 -0500

    grab all docstrings, fix some matches that were missing data

--- lib/bartleby/parser.ex
+++ lib/bartleby/parser.ex
@@ -3,64 +3,62 @@ defmodule Bartleby.Parser do
   alias Bartleby.Manifest
   alias Bartleby.Parser
   @moduledoc """
-  The parser takes a file_name as an argument and converts it to an AST using
-  `Code.string_to_quoted!`. It then parses the ast and builds a struct representing
+  The parser as a whole takes in a test or pointer to one, generates an AST using
+  `Code.string_to_quoted!`, and then it parses the ast and builds a struct representing
   the test file and its tags, cases, and assertions.
-  ##Examples
 
-    iex> File.cp("fixtures/phoenix_controller_test.txt", "fixtures/phoenix_controller_test.exs")
-    ...> test_case = Bartleby.Parser.parse("fixtures/phoenix_controller_test.exs")
-    ...> File.rm("fixtures/phoenix_controller_test.exs")
-    ...> test_case
-    %Bartleby.TestCase.File{docs: "For test purposes, here are the docs\\nsome more lines\\n",
-      module_tags: ["integration", "something_else"],
-      name: "PhoenixControllerTest",
-      test_cases: [
-        %Bartleby.TestCase.Case{
-          assertions: [
-            "assert(response.status() == 200)",
-            "assert(String.contains?(response.resp_body(), expectation_1))"],
-          name: "#POST /api/v1/the_apt returns proper response",
-          refutations: [
-            "refute(response.status() == 400)"
-          ]
-        }
-      ]
-    }
+  ##Examples
   """
 
   @spec parse_directory(String.t) :: Bartleby.Manifest.t
-  def parse_directory(dir) do
-    %Manifest{test_files: parse_files(dir)}
-  end
+  @doc "Parses a given directory's test files, outputing a manifest"
+  def parse_directory(dir), do: %Manifest{test_files: parse_files(dir)}
 
+  @doc "Parse all files in a supplied directory"
   def parse_files(dir) do
     dir
     |> File.ls!
-    |> Enum.reduce(fn (file, acc) -> [get_file("#{dir}/#{file}") | acc] end)
+    |> Enum.reduce(fn (file, acc) -> [_get_file("#{dir}/#{file}") | acc] end)
   end
 
-  def get_file(path)do
-    case File.dir?(path) do
-      true ->
-        parse_files(path)
-      false ->
-        Parser.parse(path)
-    end
-  end
+  @spec parse(String.t, List.t) :: Bartleby.TestCase.File.t
+  @doc """
+  Parses a given file path directly, returning a Batleby.TestCase.File struct with the gathered data.
+  The parser grabs all docstrings, test assertions, test case descriptions, and moduledocs.
 
-  @spec parse(String.t) :: Bartleby.TestCase.File.t
-  def parse(file_name) do
+  ## Exanmple:
+  iex> Bartleby.Parser.parse("fixtures/user_search_test.exs")
+	%Bartleby.TestCase.File{docs: ["## Simple Search\nBefore we implement a full solution with elastic search, we are building out a simple module that allows\nthe UMA app to be exposed to a very basic search interface. The goal of this interface is to search over\ntags and also by user first name, last name, or email address. It requires an exact, case sensitive match.\n",
+		"## Tag Search\nWe treat the tags a person has as if it is a database field in the sense of this search as described above.\nWe look over the exact match of a tags value. So this case largely is testing going across multiple attributes\nthat a given user has for the search\n",
+		"since we support many fields we also want to support many terms, this checks that",
+		"A group is a concept we have on the UMA and End User Sync projects. It is basically a tag `Group: foobar` that\nis searched like others but provides an interface that is needed for the user experience in both of those apps.\n"],
+	 module_tags: [], name: "UserSearchClientTest",
+	 test_cases: [%Bartleby.TestCase.Case{assertions: [],
+		 name: "finds a user if their email is given.", refutations: []},
+		%Bartleby.TestCase.Case{assertions: [],
+		 name: "finds a user if their first name is given", refutations: []},
+		%Bartleby.TestCase.Case{assertions: [],
+		 name: "finds a user if their last name is given", refutations: []},
+		%Bartleby.TestCase.Case{assertions: [],
+		 name: "can search by multiple fields (AKA by tags)", refutations: []},
+		%Bartleby.TestCase.Case{assertions: [],
+		 name: "can search by multiple fields for multiple search terms",
+		 refutations: []},
+		%Bartleby.TestCase.Case{assertions: [],
+		 name: "can offer interface to search on frontend concept of group",
+		 refutations: []}]}
+  """
+  def parse(file_name, opts \\ [])
+  def parse(file_name, _opts) do
     with {:ok, file_name} <- check_file(file_name),
          {:ok, file} <- File.read(file_name),
          {:ok, ast} <- Code.string_to_quoted(file),
          {:ok, nodes} <- get_nodes(ast) do
-
       %Bartleby.TestCase.File{
-       name: name(ast),
-       module_tags: module_tags(nodes),
-       docs: get_docs(nodes),
-       test_cases: get_test_cases(nodes)
+        name: name(ast),
+        module_tags: module_tags(nodes),
+        docs: get_docs(nodes),
+        test_cases: get_test_cases(nodes)
       }
     else {:error, error} ->
       error
@@ -79,13 +77,12 @@ defmodule Bartleby.Parser do
     end
   end
 
-  def name({:defmodule, _, [module_line | _rest]}) do
-    module_line
-    |> get_module_name
-  end
+  def name({:defmodule, _, [module_line | _rest]}), do: get_module_name(module_line)
 
   def get_module_name({:__aliases__, _line, name}) do
-    name |> List.last |> Atom.to_string
+    name
+    |> List.last
+    |> Atom.to_string
   end
 
   def module_tags (nodes) do
@@ -94,17 +91,12 @@ defmodule Bartleby.Parser do
     |> get_module_tag_names
   end
 
-  def get_module_tag_names(name_tuples) when is_list(name_tuples) do
-    name_tuples
-    |> Enum.map(&get_module_tag_names/1)
-  end
-  def get_module_tag_names({:@, _l, [{:moduletag, _l2, [name]}]}) do
-    name |> Atom.to_string
-  end
+  def get_module_tag_names(name_tuples) when is_list(name_tuples), do: Enum.map(name_tuples, &get_module_tag_names/1)
+  def get_module_tag_names({:@, _l, [{:moduletag, _l2, [name]}]}), do: Atom.to_string(name)
 
   def get_docs(nodes) do
     nodes
-    |> Enum.find(fn n -> _is_doc_node?(n) end)
+    |> Enum.reject(fn n -> !(_is_doc_node?(n)) end)
     |> _parse_doc_node
   end
 
@@ -119,7 +111,7 @@ defmodule Bartleby.Parser do
   def get_nodes({_def, _l, rest}) do
     body =
       try do
-         rest |> Enum.at(1) |> Enum.at(0)
+        rest |> Enum.at(1) |> Enum.at(0)
       catch
         _error, _ast ->
         IO.puts("Error parsing ast")
@@ -129,17 +121,15 @@ defmodule Bartleby.Parser do
     {:ok, nodes}
   end
 
-  defp _is_modult_tag_node?({:@, _l, [{:moduletag, _l2, [_text]}]}) do
-    true
-  end
+  defp _is_modult_tag_node?({:@, _l, [{:moduletag, _l2, [_text]}]}), do: true
   defp _is_modult_tag_node?(_), do: false
 
-  defp _is_doc_node?({:@, _l, [{:moduledoc, _l2, [_text]}]}) do
-    true
-  end
+  defp _is_doc_node?({:@, _l, [{:doc, _l2, [_text]}]}), do: true
+  defp _is_doc_node?({:@, _l, [{:moduledoc, _l2, [_text]}]}), do: true
   defp _is_doc_node?(_), do: false
 
   defp _parse_doc_node(node) when is_nil(node), do: []
+  defp _parse_doc_node(node) when is_list(node), do: Enum.map(node, &_parse_doc_node/1)
   defp _parse_doc_node(node) when is_tuple(node) do
     node
     |> Tuple.to_list
@@ -159,34 +149,33 @@ defmodule Bartleby.Parser do
   end
   def build_test_case(_), do: nil
 
-  def get_assertions([_name, [{_do, {_block, _meta, nodes}}]]) do
-    get_assertions(nodes)
-  end
-
+  def get_assertions([_name, [{_do, {_block, _meta, nodes}}]]), do: get_assertions(nodes)
+  def get_assertions(nodes) when is_nil(nodes), do: ""
   def get_assertions(nodes) when is_list(nodes) do
     nodes
     |> Enum.map(&get_assertion/1)
     |> Enum.filter(fn assertion -> assertion != "" end)
   end
-  def get_assertions(nodes) when is_nil(nodes), do: ""
+  def get_assertion({:assert, _line, _content} = node), do: Macro.to_string(node)
+  def get_assertion(_), do: ""
 
-  def get_refutations([_name, [{_do, {_block, _meta, nodes}}]]) do
-    get_refutations(nodes)
-  end
-
+  def get_refutations([_name, [{_do, {_block, _meta, nodes}}]]), do: get_refutations(nodes)
+  def get_refutations(nodes) when is_nil(nodes), do: ""
   def get_refutations(nodes) when is_list(nodes) do
     nodes
     |> Enum.map(&get_refutation/1)
     |> Enum.filter(fn refutation -> refutation != "" end)
   end
-  def get_refutations(nodes) when is_nil(nodes), do: ""
-
-  def get_assertion({:assert, _line, _content} = node) do
-    Macro.to_string(node)
-  end
-  def get_assertion(_), do: ""
-  def get_refutation({:refute, _line, _content} = node) do
-    Macro.to_string(node)
-  end
+  def get_refutation({:refute, _line, _content} = node), do: Macro.to_string(node)
   def get_refutation(_), do: ""
+
+  defp _get_file(path)do
+    case File.dir?(path) do
+      true ->
+        parse_files(path)
+      false ->
+        Parser.parse(path)
+    end
+  end
+
 end
